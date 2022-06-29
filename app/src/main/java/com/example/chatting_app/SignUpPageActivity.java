bf3903872d9047ed280eff6356648133db9540f1
User <alstjdwlsemr@naver.com>
2022-06-29 11:43:01 +0900

    임시 로그인 페이지까지 구현

--- app/src/main/java/com/example/chatting_app/SignUpPageActivity.java
+++ app/src/main/java/com/example/chatting_app/SignUpPageActivity.java
@@ -1,5 +1,6 @@
 package com.example.chatting_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,9 +10,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignUpPageActivity extends AppCompatActivity {
     EditText edtId, edtPw, edtName;
     Button btnSignUp2;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +31,17 @@ public class SignUpPageActivity extends AppCompatActivity {
         edtName = findViewById(R.id.edtName);
         btnSignUp2 = findViewById(R.id.btnSignUp2);
 
+        mAuth = FirebaseAuth.getInstance();
+
         btnSignUp2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email, password;
+                email = edtId.getText().toString();
+                password = edtPw.getText().toString();
+
+               createAccount(email, password);
+                /*
                 if (edtId.length() <6){
                     Toast.makeText(getApplicationContext(), "아이디의 최소 길이는 6글자입니다.", Toast.LENGTH_SHORT).show();
                     if (edtPw.length() > 20 | edtId.length() <8){
@@ -38,8 +54,34 @@ public class SignUpPageActivity extends AppCompatActivity {
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
                 }
-
+                */
             }
         });
     }
+
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(getApplicationContext(), "가입 성공", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "가입 실패.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+
+                    private void updateUI(FirebaseUser user) {
+                    }
+                });
+    }
+
+
 }
